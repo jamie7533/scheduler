c2bcd7efa8fd113c4034ea01c6e317eee7a47d0f
jamie7533 <lee.jamie.july@gmail.com>
2022-04-04 21:42:25 -0500

    modularize refactor

---
+++ src/components/CourseList.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { terms, getCourseTerm } from '../utilities/times';
+import Course from './Course.js';
+
+const TermButton = ({ term, setTerm, checked }) => (
+  <>
+    <input type="radio" id={term} className="btn-check" autoComplete="off" checked={checked}
+      onChange={() => setTerm(term)} />
+    <label className="btn btn-success m-1 p-2" htmlFor={term}>
+      {term}
+    </label>
+  </>
+); // changed to "label className="btn..." (different from tutorial which just had 'class'; typo?)
+
+const TermSelector = ({ term, setTerm }) => (
+  <div className="btn-group">
+    {
+      Object.values(terms)
+        .map(value => <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />)
+    }
+  </div>
+);
+
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState('Fall');
+  const [selected, setSelected] = useState([]);
+  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+
+  return (
+    <>
+      <TermSelector term={term} setTerm={setTerm} />
+      <div className="course-list">
+        {termCourses.map(course =>
+          <Course key={course.id} course={course}
+            selected={selected} setSelected={setSelected}
+          />)}
+      </div>
+    </>
+  );
+};
+
+export default CourseList;
