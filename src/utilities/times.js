6bdac1780e2db2b9ed657c9e70f660d123a9be02
jamie7533 <lee.jamie.july@gmail.com>
2022-04-06 23:27:44 -0500

    write to firebase

--- src/utilities/times.js
+++ src/utilities/times.js
@@ -2,6 +2,19 @@
 
 export const terms = { F: 'Fall', W: 'Winter', S: 'Spring' };
 
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+export const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+
 export const getCourseTerm = course => (
   terms[course.id.charAt(0)]
 );
